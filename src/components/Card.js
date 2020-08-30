0e39484ea2d2e81b30489f52b0c52bbfc2a734fe
Natalia Pavlova <natalja1912@yandex.ru>
2020-08-30 22:30:00 +0300

    feat: cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 export default class Card {
   constructor(item, cardSelector) {
-    this._link = item.link;
+    this._subheading = item.subheading;
     this._text = item.text;
-    this._price = item.price;
-    this._likes = item.likes;
-    this._messages = item.messages;
+    this._size = item.size;
+    this._descriptionText = item.description;
+    this._disabled = item.disabled;
     this._cardSelector = cardSelector;
   }
 
@@ -16,38 +16,67 @@ export default class Card {
 
   generateCard() {
     this._element = this._getTemplate();
-    this._priceText = this._element.querySelector('.card__price');
-    this._addButton = this._element.querySelector('.card__add-button');
-    this._likesText = this._element.querySelector('.card__likes');
-    this._messagesText = this._element.querySelector('.card__messages');
-    this._element.querySelector('.card__image').src = this._link;
-    this._element.querySelector('.card__title').textContent = this._text;
-    this._priceText.textContent = this._price;
-    this._likesText.textContent = this._likes;
-    this._messagesText.textContent = this._messages;
-    this._addButton.classList.toggle('card__display-none');
-    this._likesText.classList.toggle('card__display-none');
-    this._messagesText.classList.toggle('card__display-none');
-    this._setEventListeners();
+    this._description = this._element.querySelector('.card__description');
+    this._heading = this._element.querySelector('.card__heading');
+    this._action = this._element.querySelector('.card__action');
+    this._html = this._action.innerHTML;
+    this._span = this._element.querySelector('.card__span');
+    this._element.querySelector('.card__subheading').textContent = this._subheading;
+    this._element.querySelector('.card__text').textContent = this._text;
+    this._element.querySelector('.card__size-digits').textContent = this._size;
+    this._card = this._element.querySelector('.card');
+
+    if (this._disabled){
+      this._action.classList.add('card__action_type_disabled');
+      this._card.classList.add('card_disabled');
+      this._element.querySelector('.card__size').classList.add('card__size_type_disabled');
+      this._action.textContent = `Печалька, ${this._subheading} закончился`;
+    }
+    
+    if (!this._disabled){
+      this._setEventListeners();
+    }
+
     return this._element;
   }
 
   _setEventListeners() {
-    this._card = this._element.querySelector('.card');
-    this._card.addEventListener('mouseover', this._toggleDisplay);
-    this._card.addEventListener('mouseout', this._toggleDisplay);
+    this._card.addEventListener('click', this._toggleCard);
   }
 
-  _toggleDisplay = (evt) => {
-      this._addButton.classList.toggle('card__display-none');
-      this._likesText.classList.toggle('card__display-none');
-      this._messagesText.classList.toggle('card__display-none');
-      if (!this._addButton.classList.contains('card__display-none')){
-        this._priceText.style.color = "#8c84f9";
+  _toggleCard = (evt) => {
+    this._card.classList.toggle('card_active');
+    this._card.querySelector('.card__size').classList.toggle('card__size_type_active');
+
+    if (this._card.classList.contains('card_active')) {
+      this._action.textContent = this._descriptionText;
+      if (!this._action.contains(evt.target)) {
+        this._description.textContent = "Котэ не одобряет?";
+        this._description.classList.add('card__description_type_hover');
       }
-      else {
-        this._priceText.style.color = "#333333";
-      }
+      this._card.addEventListener('mouseover', this._changeHeading);
+      this._card.addEventListener('mouseout', this._changeHeading);
+    }
+    else {
+      this._description.classList.remove('card__description_type_hover');
+      this._description.textContent = "Сказочное заморское яство";
+      this._action.innerHTML = this._html;
+      this._card.removeEventListener('mouseover', this._changeHeading);
+      this._card.removeEventListener('mouseout', this._changeHeading);
+    }
+  }
+
+  _changeHeading = (evt) => {
+    let text = this._description.textContent;
+    if (text === "Котэ не одобряет?"){
+      this._description.textContent = "Сказочное заморское яство";
+      this._description.classList.remove('card__description_type_hover');
+    }
+    else if (!this._action.contains(evt.target)) {
+      this._description.textContent = "Котэ не одобряет?";
+      this._description.classList.add('card__description_type_hover');
+    }
+
   }
 
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import { cardBags, watches, mobile, toys, cloth, fashion, furniture } from '../utils/constants.js';
+import { cards } from '../utils/constants.js';
 import Card from '../components/Card.js';
 
 //функция для отрисовки карточек
@@ -12,32 +12,7 @@ function renderCards(cards, containerSelector) {
     })
 }
 
-//отрисовка карточек с сумками на странице
-renderCards(cardBags, '.cards');
+//отрисовка карточек на странице
+renderCards(cards, '.cards');
 
-//отрисовка карточек с часами на странице
-renderCards(watches, '.watches__cards');
 
-//массив со всеми товарами
-const allCards = [watches, mobile, toys, cloth, fashion, furniture];
-
-const navList = document.querySelectorAll('.watches__nav-item');
-
-//отрисовка карточек по ховеру на нужную категорию товаров
-navList.forEach((item) => {
-    item.addEventListener('mouseover', () => {
-        document.querySelector('.watches__nav-item-watches').classList.remove('watches__nav-item_type_active');
-        item.classList.add('watches__nav-item_type_active');
-        const name = item.textContent;
-        allCards.forEach((list) => {
-            const type = list[0].type;
-            if (type === name) {
-                document.querySelector('.watches__cards').innerHTML = '';
-                renderCards(list, '.watches__cards');
-            }
-        })
-    })
-    item.addEventListener('mouseout', () => {
-        item.classList.remove('watches__nav-item_type_active');
-    })
-})
